66cf2124b47684b742966782bdd306cc077a5163
everis.bruno.rodrigues.gil@externo.bancobpi.pt <everis.bruno.rodrigues.gil@externo.bancobpi.pt>
2022-04-04 10:41:12 +0100

    Testing a new rule

---
+++ functions/http-erros-code-reference.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = (input, options, context) => {
+  const path = context.path.toString();
+  const pathParameters = path.split(",");
+  const httpCode = pathParameters[4];
+  const url = "error-block-".concat(httpCode);
+  const includes = input.includes(url);
+
+  if(includes){
+    return [ ]
+  }else{
+    return [
+      {
+        message: "Your http code " + httpCode + " MUST be Design Library reference to " + url
+      }
+    ]
+  }
+};
