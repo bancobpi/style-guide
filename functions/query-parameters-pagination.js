a5108a4e3da6cb4603f93651e75da926f767cf49
everis.bruno.rodrigues.gil@externo.bancobpi.pt <everis.bruno.rodrigues.gil@externo.bancobpi.pt>
2022-06-01 15:03:50 +0100

    Modified functions/query-parameters-pagination.js

--- functions/query-parameters-pagination.js
+++ functions/query-parameters-pagination.js
@@ -1,26 +1,40 @@
 'use strict';
 
-module.exports = (targetValue) => {
-  if (!Array.isArray(targetValue)) {
-    return [
-      {
-        message: `No array given, provide $.paths.*.*.parameters`,
-      },
-    ];
+export default (targetValue,options, context) => {
+  const result = [];
+
+  if (targetValue === null || typeof targetValue !== 'object') {
+    return result;
   }
 
-  const pagintationParams = targetValue.filter(
-    (param) =>
-      param.name && (param.in === 'query' && (param.name.toLowerCase() === 'pagenumber' || param.name.toLowerCase() === 'pagesize')),
-  );
+  const path = context.path.toString().split(",");
+  const verb = path[2];
+  const splitId = path[1].split("/");
+  var name = path[2];
 
-  if (pagintationParams.length !== 2) {
-    return [
-      {
-        message: `GET List MUST have a queryParameters of pagination.`,
-      },
-    ];
+  if (verb == "get" || verb == "post") {
+    if (splitId.length == 3) {
+      name = verb + "ById";
+    }else if (splitId.length == 4) {
+      name = verb + "WithId";
+    }
   }
+
+  if(!name.includes("ById")){
+    const pagintationParams = targetValue.filter(
+      (param) =>
+        param.name && (param.in === 'query' && (param.name.toLowerCase() === 'pagenumber' || param.name.toLowerCase() === 'pagesize')),
+    );
+
+    if (pagintationParams.length !== 2) {
+      return [
+        {
+          message: `GET List MUST have a queryParameters of pagination.`,
+        },
+      ];
+    }
+  }
+  
 
   return [];
 };
