8e61714cda58ea2117a2fd8e54f66f85c6501709
everis.bruno.rodrigues.gil@externo.bancobpi.pt <everis.bruno.rodrigues.gil@externo.bancobpi.pt>
2022-07-14 08:51:48 +0100

    New rule to validate common-body and change the rule to validate query-parameters

--- functions/query-parameters-pagination.js
+++ functions/query-parameters-pagination.js
@@ -1,25 +1,38 @@
+
 'use strict';
 
 export default (targetValue,options, context) => {
   const path = context.path.toString().split(",");
   const verb = path[2];
-  const splitId = path[1].split("/");
+  const splitId = path[1].split("/");   
   var name = path[2];
   const result = [];
+  const query = targetValue.parameters;
 
   if (verb == "get" && splitId.length != 3){
-    const pagintationParams = targetValue.filter(
-      (param) =>
-        param.name && (param.in === 'query' && (param.name.toLowerCase() === 'pagenumber' || param.name.toLowerCase() === 'pagesize')),
-    );
-
-    if (pagintationParams.length !== 2) {
-      return [
-        {
-          message: `GET List MUST have a queryParameters of pagination.`,
-        },
-      ];
+    if(query != undefined){
+     const pagintationParams = query.filter(
+        (param) =>
+          (param.in === 'query' && (param.name.toLowerCase() === 'pagenumber' || param.name.toLowerCase() === 'pagesize')) 
+      );
+      if (pagintationParams.length == 2) {
+        if(pagintationParams[0].name == pagintationParams[1].name){
+          result.push({
+            message: "GET List MUST have pagination query parameters 'pageNumber' and 'pageSize'."
+          });
+        }else{
+          return result;
+        } 
+      }else{
+        result.push({
+          message: "GET List MUST have pagination query parameters 'pageNumber' and 'pageSize'."
+        });
+      }
+    }else{
+      result.push({
+        message: "GET List MUST have pagination query parameters 'pageNumber' and 'pageSize'."
+      });
     }
   }
-  return [];
+  return result;
 };
