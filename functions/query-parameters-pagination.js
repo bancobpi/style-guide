e22da02023749c1feb1783b0acb89876918a1d8f
paulosantosbpiext <deloitte.paulo.miguel.santos@externo.bancobpi.pt>
2024-09-11 14:42:03 +0100

    pageNumber and pageSize must be required or have a default value

--- functions/query-parameters-pagination.js
+++ functions/query-parameters-pagination.js
@@ -22,12 +22,12 @@ export default (targetValue,options, context) => {
             message: "GET List MUST have pagination query parameters 'pageNumber' and 'pageSize'."
           });
         }else{
-          if(pagintationParams[0].required && pagintationParams[1].required) {
+          if((pagintationParams[0].required || pagintationParams[0].schema.default) && (pagintationParams[1].required || pagintationParams[1].schema.default)) {
             return result;
           }
           else {
             result.push({
-              message: "'pageNumber' and 'pageSize' MUST be required."
+              message: "'pageNumber' and 'pageSize' MUST be required or have a default value."
             });
           }
         } 
